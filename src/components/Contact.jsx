230ad3fe620916609ffe557eaf558b8e319d11e2
Hasin <121337662+hasin-riki@users.noreply.github.com>
2023-12-27 02:57:18 +0500

    contact + emailjs + earth + stars + env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,36 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID, 
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
+        { 
+          from_name: form.name,
+          to_name: 'Hasin',
+          from_email: form.email,
+          to_email: 'h.zaman.23010@khi.iba.edu.pk',
+          message: form.message
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+
+      setLoading(false);
+      alert('Thank you! I will get back to you as soon as possible!');
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      alert('Something went wrong.');
+    }
   };
 
   return (
@@ -75,6 +104,12 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+      <motion.div 
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   )
