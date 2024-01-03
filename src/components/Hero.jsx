3ab94bf6f46af27636885d0170ac99a5d72991cc
Hasin <121337662+hasin-riki@users.noreply.github.com>
2024-01-03 21:44:22 +0500

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} inset-0 absolute top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center mt-10">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+          <div className="w-1 sm:h-80 h-50 violet-gradient"/>
         </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hello <span className="text-[#915eff]">World!</span></h1>
-          <p className={`${styles.heroSubText} text-white mt-2`}>I'm <span className="text-[#915eff]">Hasin.</span> A passionate full-stack software engineer.</p>
+        <div className="mt-10">
+          <h1 className={`${styles.heroHeadText} text-white`}>Hello World! <br />I'm <span className="text-[#915eff]">Hasin.</span></h1>
+          <p className={`${styles.heroSubText} text-white mt-2`}>A passionate full-stack software engineer. I believe coding can be an art and coders can be artists. ✨✨</p>
         </div>
       </div>
       {/* <ComputersCanvas /> */}
